a7af57a192d6c5aa05221e023a29369a5550a38e
Alexander Anpleenko <vaeum@yahoo.com>
2016-10-23 12:35:08 +0300

    add webpack

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import * as icon from 'react-open-iconic-svg';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+import './style.scss';
+
+const cn = require('bem-cn')('icons');
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copiedId: null,
+      showNote: false
+    }
+  }
+
+  renderIcon() {
+    var arr = [];
+
+    for (var key in icon) {
+      arr.push(icon[key])
+    }
+
+    return arr;
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <h1>react-octicons-svg</h1>
+        </div>
+
+        <div className={cn('note').state({show: this.state.showNote})}>
+          <span>Copied</span>
+        </div>
+
+        <ul className={cn('list').mix('row')}>
+          {this.renderIcon().map((e, i) =>
+             <li className={cn('list-item')} key={i}>
+                <CopyToClipboard text={`<${e.name} />`}
+                  onCopy={() => this.setState({copied: true})}>
+
+                  <div
+                    className={cn('link').state({copied: this.state.copiedId == i})}
+                    onClick={() => this.handerClick(i)} >
+
+                    <span className={cn('icon')}>{React.createElement(e)}</span>
+                    <span className={cn('icon-description')}>{e.name}</span>
+                  </div>
+                </CopyToClipboard>
+             </li>
+           )}
+        </ul>
+      </div>
+    );
+  }
+
+  handerClick(id){
+    this.setState({
+      copiedId: id,
+      showNote: true
+    })
+
+    let hideNote = () => {
+      this.setState({
+        showNote: false
+      })
+    }
+
+    setTimeout(hideNote, 2000)
+
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var AUTOPREFIXER_BROWSERS = [
+  'Android 2.3',
+  'Android >= 4',
+  'Chrome >= 35',
+  'Firefox >= 31',
+  'Explorer >= 9',
+  'iOS >= 7',
+  'Opera >= 12',
+  'Safari >= 7.1',
+];
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'postcss-loader', 'sass']
+    }]
+  },
+  postcss: function () {
+    return [
+      require('autoprefixer')({ browsers: AUTOPREFIXER_BROWSERS })
+    ];
+  }
+};
